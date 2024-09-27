463e66409f2e576f95a47d3207de91b04a48d6c5
Voit <soloyjon27@gmail.com>
2024-09-26 20:52:45 -0400

    white window fix

--- src/setupDriver.js
+++ src/setupDriver.js
@@ -4,10 +4,11 @@ async function setupDriver() {
 
     let options = new chrome.Options();
 
-    options.addArguments('--headless');
+    options.addArguments('--headless=old');
     options.addArguments('--disable-dev-shm-usage');
     options.addArguments('--mute-audio');
     options.addArguments("--log-level=3");
+    options.addArguments("--disable-software-rasterizer");
     //options.addArguments("--incognito");
     options.addArguments('--disable-gpu');
     options.addArguments('--no-sandbox');
